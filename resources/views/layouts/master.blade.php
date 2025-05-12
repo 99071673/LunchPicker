40b398a0a8936b850cfa9664de4e1f2e48490521
Jelle Fiere <113093557+pinglecan@users.noreply.github.com>
2025-05-12 13:43:40 +0200

    added tainwind into the master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lunchpicker</title>
+    @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     @stack('styles')
 </head>
 <body>
 
     <header>
-        <div class="logo">Lunchpicker</div>
+        <div class="font-bold text-2xl">Lunchpicker</div>
     </header>
 
     <main class="content">
