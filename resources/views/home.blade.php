3805b3faff38786acadbb4748436233d77dadab6
Finn <113095872+FinnSeton@users.noreply.github.com>
2025-05-13 11:56:12 +0200

    Feature/header 2 (#11)
    * Add: files of 0393ea2
    
    * test
    
    ---------
    
    Co-authored-by: Yuhki <99071673@mydavinci.nl>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,22 @@
 @extends('layouts.master')
 
 @section('pagetitle') Home @endsection
+
+@section('content')
+    <div>
+        <div class="w-[1700px] p-4 flex flex-col gap-5">
+
+            <div class="grid grid-cols-[1fr_2fr_1fr] gap-10">
+                <div class="bg-white border rounded shadow p-4 h-[650px] w-full">
+
+                </div>
+                <div class="bg-white border rounded shadow p-4 h-[650px] w-full">
+
+                </div>
+                <div class="bg-white border rounded shadow p-4 h-[650px] w-full">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
