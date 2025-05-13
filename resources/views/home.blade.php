1252b15152b354235c0799dedc943489afb28c44
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2025-05-13 11:53:50 +0200

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,21 @@
 
 @section('pagetitle') Home @endsection
 
-<div>
-    <div class="w-[1200px] p-4 flex flex-col gap-5">
+@section('content')
+    <div>
+        <div class="w-[1700px] p-4 flex flex-col gap-5">
 
-        <div class="bg-white border rounded shadow p-4 h-[100px]">
-        </div>
+            <div class="grid grid-cols-[1fr_2fr_1fr] gap-10">
+                <div class="bg-white border rounded shadow p-4 h-[650px] w-full">
 
-        <div class="grid grid-cols-2 gap-10">
-            <div class="bg-white border rounded shadow p-4 h-[350px]">
+                </div>
+                <div class="bg-white border rounded shadow p-4 h-[650px] w-full">
+
+                </div>
+                <div class="bg-white border rounded shadow p-4 h-[650px] w-full">
+
+                </div>
             </div>
-            <div class="bg-white border rounded shadow p-4 h-[350px]">
-            </div>
         </div>
-
     </div>
-</div>
+@endsection
