59de5f8cb93696fa789cdb769e48f0b650ce79ef
Yuhki <99071673@mydavinci.nl>
2025-05-13 12:10:12 +0200

    Rebase: 13 May

---
+++ storage/framework/views/9c8b11aaea24d641086678331f387856.php
@@ -0,0 +1,2 @@
+<div <?php echo e($attributes->class('flex-1')); ?> data-flux-spacer></div>
+<?php /**PATH C:\Users\YukiT\OneDrive - Da Vinci College\FASE 3\Lunch Picker App\LunchPicker\vendor\livewire\flux\stubs\resources\views\flux\spacer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c9e17bf6f022b93beffee8a711f12d3d.php
@@ -0,0 +1,6 @@
+
+<ui-menu-radio-group <?php echo e($attributes); ?> data-flux-menu-radio-group>
+    <?php echo e($slot); ?>
+
+</ui-menu-radio-group>
+<?php /**PATH C:\Users\YukiT\OneDrive - Da Vinci College\FASE 3\Lunch Picker App\LunchPicker\vendor\livewire\flux\stubs\resources\views\flux\menu\radio\group.blade.php ENDPATH**/ ?>
