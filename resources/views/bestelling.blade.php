ca9c8930c40aa1c104c9d82383690c3fa94f496a
MilanoGamezzYT <99067157@mydavinci.nl>
2025-05-13 12:07:49 +0200

    add: knop voor items toevoegen

--- resources/views/bestelling.blade.php
+++ resources/views/bestelling.blade.php
@@ -13,8 +13,10 @@
         </div>
 
         <div class="grid grid-cols-2 gap-10">
-            <div class="bg-white border rounded shadow p-4 h-[350px] flex flex-col justify-start items-center">
+            <div class="bg-white border rounded shadow p-4 h-[350px] flex flex-col justify-start items-center relative">
                 <p class="text-3xl font-semibold mt-5">Selecteer je lunch</p>
+
+                <button class="bg-blue-500 text-white py-2 px-6 rounded-full hover:bg-blue-600 transition-all absolute bottom-4 right-4">Voeg nieuw item toe</button>
             </div>
             <div class="bg-white border rounded shadow p-4 h-[350px] flex flex-col justify-start items-center relative">
                 <p class="text-3xl font-semibold mt-5 border-b-2 border-black w-[75%] text-center pb-2">Jouw huidige bestelling</p>
