6ce25fdf43c4c608971536c05679a2322c7fb341
Yuhki <99071673@mydavinci.nl>
2025-05-19 00:36:26 +0200

    Add: test stappen

--- run.ps1
+++ run.ps1
@@ -1,24 +1,16 @@
 Rename-Item -Path ".env.example" -NewName ".env"
 
-php artisan migrate
-
 composer install
-composer update
 
-composer dump-autoload
+npm install
+npm run build
 
-php artisan config:clear
-php artisan cache:clear
-php artisan optimize
 php artisan key:generate
-php artisan config:cache
 
 php artisan migrate
+
+php artisan config:cache
+php artisan cache:clear
 
 php -S localhost:44302 -t public
-if ($LASTEXITCODE -ne 0) {  Write-Host "Laravel server failed to start!"
-    pause
-    exit
-}
-
 pause
