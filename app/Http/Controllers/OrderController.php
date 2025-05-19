c80b7d0ad060c80064e253634cb7554dad4e9a2b
MilanoGamezzYT <99067157@mydavinci.nl>
2025-05-19 14:03:34 +0200

    OrderController updated

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,18 +8,69 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function show(Request $request, $location_id)
+    public function show($location_id)
     {
         $location = Location::findOrFail($location_id);
-
-        $query = $request->input('search');
-
-        $lunchItems = LunchItem::where('location_id', $location_id)
-            ->when($query, function ($q) use ($query) {
-                return $q->where('naam', 'like', '%' . $query . '%');
-            })
-            ->get();
+        $lunchItems = LunchItem::where('location_id', $location_id)->get();
 
         return view('order.show', compact('location', 'lunchItems'));
+    }
+
+    public function add(Request $request)
+    {
+        $item = LunchItem::findOrFail($request->input('item_id'));
+
+        $order = session()->get('order', []);
+        
+        $bestaat = false;
+        foreach ($order as &$orderItem) {
+            if ($orderItem['id'] == $item->id) {
+                $orderItem['aantal'] = ($orderItem['aantal'] ?? 1) + 1;
+                $bestaat = true;
+                break;
+            }
+        }
+
+        if (!$bestaat) {
+            $order[] = [
+                'id' => $item->id,
+                'naam' => $item->naam,
+                'prijs' => $item->prijs,
+                'aantal' => 1,
+            ];
+        }
+
+        session()->put('order', $order);
+
+        return back();
+    }
+
+    public function remove(Request $request)
+    {
+        $order = session()->get('order', []);
+        $key = $request->input('key');
+
+        if (isset($order[$key])) {
+            unset($order[$key]);
+            $order = array_values($order);
+            session()->put('order', $order);
+        }
+
+        return back();
+    }
+
+    public function update(Request $request)
+    {
+        $key = $request->input('key');
+        $aantal = (int) $request->input('aantal');
+
+        $order = session()->get('order', []);
+
+        if (isset($order[$key]) && $aantal > 0) {
+            $order[$key]['aantal'] = $aantal;
+            session()->put('order', $order);
+        }
+
+        return back();
     }
 }
