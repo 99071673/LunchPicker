9cd9eddf145d28894047904f23d820e4a310cee9
MilanoGamezzYT <99067157@mydavinci.nl>
2025-05-20 13:09:27 +0200

    nieuwe clone, branch en updates files

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,23 @@ use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 use App\Http\Controllers\TimerController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\LunchItemController;
 
 Route::get('/', [TimerController::class, 'home'])->name('home');;
 
 Route::get('/userprofile', function () {
     return view('userprofile');
 })->name('userprofile');
+
+Route::get('/bestellen/{location_id}', [OrderController::class, 'show'])->name('bestelling');
+
+Route::get('/lunchitems/create', [LunchItemController::class, 'create'])->name('lunchitems.create');
+Route::post('/lunchitems', [LunchItemController::class, 'store'])->name('lunchitems.store');
+
+Route::post('/order/add', [OrderController::class, 'add'])->name('order.add');
+Route::post('/order/update', [OrderController::class, 'update'])->name('order.update');
+Route::post('/order/remove', [OrderController::class, 'remove'])->name('order.remove');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
