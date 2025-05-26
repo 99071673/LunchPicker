e76abc3e3dbeba5ee8690f208bc69356e6fb0aac
Yuhki <99071673@mydavinci.nl>
2025-05-26 19:14:15 +0200

    Add: Admin panel & redirects login for unauthenticated & admin role

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Location;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display the admin dashboard.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $users = User::all();
+        $locations = Location::all();
+
+        return view('admin', compact('users', 'locations'));
+    }
+}
---
+++ resources/views/admin.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.master')
+
+@section('pagetitle') Admin @endsection
+
+@section('content')
+    <div class="max-w-screen-xl w-full mx-auto p-4 flex flex-col gap-5">
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-7 items-stretch">
+
+            <div class="flex flex-col justify-between h-[650px] w-full gap-6">
+
+                <div class="bg-white border rounded-lg shadow p-4 flex-1 flex flex-col">
+                    <div class="border-b-4 border-black mb-4">
+                        <p class="text-4xl font-bold flex justify-center">Deadlines</p>
+                    </div>
+                    <div class="flex-1 overflow-y-auto">
+                    </div>
+                </div>
+
+                <div class="bg-white border rounded-lg shadow p-4 flex-1 flex flex-col">
+                    <div class="border-b-4 border-black mb-4">
+                        <p class="text-4xl font-bold flex justify-center">Gebruikers</p>
+                    </div>
+                    <div class="flex-1 overflow-y-auto">
+                        @if($users->isEmpty())
+                            <p class="text-center text-gray-500">Geen gebruikers gevonden</p>
+                        @else
+                            <ul class="space-y-2">
+                                @foreach($users as $user)
+                                    <li class="p-2 border-b">
+                                {{-- Hier moeten we nog even naar gaan kijken of we nou email of naam (of allebei) willen gaan tonen --}}
+                                        <p class="font-bold">{{ $user->name }}</p>
+                                        <p class="text-sm text-gray-600">{{ $user->email }}</p>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="bg-white border rounded-lg shadow p-4 h-[650px] w-full flex flex-col">
+                <div class="border-b-4 border-black mb-4">
+                    <p class="text-4xl font-bold flex justify-center">Locaties</p>
+                </div>
+                <div class="flex-1 overflow-y-auto">
+                    @if($locations->isEmpty())
+                        <p class="text-center text-gray-500">Geen locaties gevonden</p>
+                    @else
+                        <ul class="space-y-2">
+                            @foreach($locations as $location)
+                                <li class="p-2 border-b">
+                                    <p class="font-bold">{{ $location->name }}</p>
+                                    <p class="text-sm text-gray-600">{{ $location->address }}</p>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div>
+            </div>
+
+            <div class="bg-white border rounded-lg shadow p-4 h-[650px] w-full flex flex-col">
+                <div class="border-b-4 border-black mb-4">
+                    <p class="text-4xl font-bold flex justify-center">Overig</p>
+                </div>
+                <div class="flex-1 overflow-y-auto">
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
