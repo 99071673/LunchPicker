21fddb7723d50e0920cc8b9eede6a9621767383e
Yuhki <143695253+99071673@users.noreply.github.com>
2025-05-27 10:49:01 +0200

    Add: AdminUserSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             LocationSeeder::class,
+            AdminUserSeeder::class,
         ]);
 
         User::factory()->create([
