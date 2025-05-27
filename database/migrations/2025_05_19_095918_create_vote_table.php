319ffea59cbb6b935765740c1dce02de28c300de
Jelle Fiere <113093557+pinglecan@users.noreply.github.com>
2025-05-27 10:30:14 +0200

    a user can vote only one time

--- database/migrations/2025_05_19_095918_create_vote_table.php
+++ database/migrations/2025_05_19_095918_create_vote_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,10 +13,20 @@ return new class extends Migration
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignId('location_id')->references('id')->on('locations')->onDelete('cascade');
-            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->foreignId('location_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            // Ensure one vote per user
+            $table->unique('user_id');
         });
     }
+
 
     /**
      * Reverse the migrations.
