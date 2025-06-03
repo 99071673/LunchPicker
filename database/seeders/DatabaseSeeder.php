070a2a426cc4b38543f81ec89d77937340b509f1
Finn <113095872+FinnSeton@users.noreply.github.com>
2025-06-03 11:52:38 +0200

    seeder missing (#48)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LocationSeeder::class,
             AdminUserSeeder::class,
+            DeadlineSeeder::class,
         ]);
 
         User::factory()->create([
