2aade02dae6e7adc0759976bafc0b3c0390f4b7c
Milan <113093918+MilanoGamezzYT@users.noreply.github.com>
2025-06-16 11:16:35 +0200

    deadline op order page (#53)
    Co-authored-by: Yuhki <143695253+99071673@users.noreply.github.com>

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.master')
 
 @section('pagetitle')
-    Deadline
+    <div class="flex flex-col items-center">
+        @if($status === 'bestellen')
+            <x-countdown-timer class="text-3xl font-bold" :deadline="$orderdeadline" id="home-timer" />
+        @endif
+    </div>
 @endsection
 
 @section('content')
